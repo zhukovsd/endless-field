4954f22ec64ae097ed348379deae1bbe08dc1aa6
zhukovsd <zhukovsd@gmail.com>
2016-05-25 14:29:55 +0300

    CameraPosition.shiftBy, calculateMouseOffset

--- web/js/CameraPosition.js
+++ web/js/CameraPosition.js
@@ -29,15 +29,43 @@ CameraPosition.prototype = {
         
         var originChunkId = this.originChunkId;
         var x = this.shift.x - mouseOffset.x;
-        var y = this.shift.y;
-        
+        var y = this.shift.y - mouseOffset.y;
+
+        // camera shifted beyond the right boundary of chunk
         if (x > chunkWidthInPixels) {
             originChunkId += Math.floor(x / chunkWidthInPixels);
-            x = x % chunkWidthInPixels;
+            x %= chunkWidthInPixels;
+        // camera shifted beyond the left boundary of chunk
         } else if (x < 0) {
+            originChunkId -= Math.ceil(Math.abs(x) / chunkWidthInPixels);
+            x = chunkWidthInPixels + (x % chunkWidthInPixels);
+        }
 
+        // camera shifted beyond the bottom boundary of chunk
+        if (y > chunkHeightInPixels) {
+            originChunkId += Math.floor(y / chunkHeightInPixels) * chunkIdFactor;
+            y %= chunkHeightInPixels;
+        // camera shifted beyond the top boundary of chunk
+        } else if (y < 0) {
+            originChunkId -= Math.ceil(Math.abs(y) / chunkHeightInPixels) * chunkIdFactor;
+            y = chunkHeightInPixels + (y % chunkHeightInPixels);
         }
         
         return new CameraPosition(originChunkId, x, y);
+    },
+
+    calculateMouseOffset: function(position, chunkSize, chunkIdFactor, cellSize) {
+        var chunkWidthInPixels = chunkSize.columnCount * cellSize.width;
+        var chunkHeightInPixels = chunkSize.rowCount * cellSize.height;
+
+        var hChunkOffset = (this.originChunkId % chunkIdFactor - position.originChunkId % chunkIdFactor) * chunkWidthInPixels;
+        var hOffset = hChunkOffset + this.shift.x - position.shift.x;
+
+        var vChunkOffset = (
+            Math.floor(this.originChunkId / chunkIdFactor) - Math.floor(position.originChunkId / chunkIdFactor)
+        ) * chunkHeightInPixels;
+        var vOffset = vChunkOffset + this.shift.y - position.shift.y; 
+        
+        return {x: hOffset, y: vOffset};
     }
 };
