1f1033651baeb6646089e42228bc35f5550cd3ae
zhukovsd <zhukovsd@gmail.com>
2016-07-05 12:55:39 +0300

    Related keys mechanism, null mock values
    Values may be dependant from another value. If value by key 0 needs access to value by key 1, then value by key 1 have to be instantiated first, then we postpone instantiating of value by key 0 until value by key 1 not attempted to instantiate.  That is, any key has set of related keys, which affects instantiation order.
    
    If any value failed to be instantiated, null mock object placed by that key. That way we not have to call instantiator repetitively just to check that value is still can't be instantiated.
    
    New types introduced:
    InstantiationData - data necessary for instantiating new value. if value is currently null, if it is related or not, and set of already locked keys
    ValueInstantiator - functional interface, producing values by key and instantiation data object
    InstantiationResultType - enum will possible instantiation result types, created value, null value, instantiating postponed due to lacking of related values
    InstantiationResult - object with instantiation result, contains result type and value, if provided

---
+++ src/main/java/com/zhukovsd/entrylockingconcurrenthashmap/InstantiationData.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2016 Zhukov Sergei
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.zhukovsd.entrylockingconcurrenthashmap;
+
+import java.util.Set;
+
+/**
+ * Created by ZhukovSD on 25.06.2016.
+ */
+public class InstantiationData<K> {
+    public final boolean isRelated;
+    public final boolean isNull;
+    public final boolean isReproviding;
+    public final Set<K> lockedKeys;
+
+    InstantiationData(boolean isRelated, boolean isNull, boolean isReproviding, Set<K> lockedKeys) {
+        this.isRelated = isRelated;
+        this.isNull = isNull;
+        this.isReproviding = isReproviding;
+        this.lockedKeys = lockedKeys;
+    }
+}
