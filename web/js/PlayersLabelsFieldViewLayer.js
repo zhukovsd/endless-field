f572b57535d95a38d56ac7594476a9959d73541b
zhukovsd <zhukovsd@gmail.com>
2016-09-10 18:40:09 +0300

    A new approach to render layers
    Now, layer render to the buffer canvas, which size is larger than the size of visible GUI canvas. Visible canvas displays part of buffer canvas. Rendering happens only when data changes, not when a user scrolls the camera. This approach avoids render during mouse move, which is more performant, especially in the Firefox.

--- web/js/PlayersLabelsFieldViewLayer.js
+++ web/js/PlayersLabelsFieldViewLayer.js
@@ -21,47 +21,79 @@
 var PlayersLabelsFieldViewLayer = function(fieldView, canvasId) {
     AbstractFieldViewLayer.call(this, fieldView, canvasId);
 
-    this.drawVisiblePlayersLabels = function() {
-        this.clear();
-        var scope = this.fieldView.camera.cellsScope();
-
-        for (var userId in this.fieldManager.playersPositions) {
-            if (this.fieldManager.playersPositions.hasOwnProperty(userId)) {
-                var position = this.fieldManager.playersPositions[userId].position;
-                var player = this.fieldManager.playersPositions[userId].player;
-
-                if (scope.containsCell(position.row, position.column)) {
-                    // console.log(this.fieldManager.players[userId].name);      
-                    this.drawPlayerLabel(position, player.name);
-                }
-            }
-        }
-    }
+    this.mouseListener = null;
+    this.renderedRects = [];
 };
 
 PlayersLabelsFieldViewLayer.prototype = Object.create(AbstractFieldViewLayer.prototype);
 
-PlayersLabelsFieldViewLayer.prototype.drawByPositions = function(positions) {
-    this.clear();
-    this.drawVisiblePlayersLabels();
-};
-
-PlayersLabelsFieldViewLayer.prototype.drawPlayerLabel = function(cellPosition, name) {
-    var c = this.context;
-    var cellRect = this.fieldView.camera.cellRect(cellPosition.row, cellPosition.column);
-
-    c.strokeStyle = "white";
+PlayersLabelsFieldViewLayer.prototype.initRenderCanvasStyleSettings = function() {
+    // initialize render canvas with styles to render player label text
+    var c = this.imageData.renderContext;
+    c.fillStyle = "white";
     c.lineWidth = 0;
     c.font = "bold 15px Arial";
-    c.fillStyle = "rgba(0, 0, 0, 0.5)";
-    // c.fillStyle = "black";
+};
 
-    var rect = {x: cellRect.x - 0.5, y: cellRect.y - 20.5, width: c.measureText(name).width + 10, height: 20};
+inheritedRenderByChunkIds = PlayersLabelsFieldViewLayer.prototype.renderByChunkIds;
+PlayersLabelsFieldViewLayer.prototype.renderByChunkIds = function(chunkIds) {
+    this.renderedRects = [];
+    inheritedRenderByChunkIds.call(this, chunkIds);
+};
 
-    // c.beginPath();
-    c.fillRect(rect.x, rect.y, rect.width, rect.height);
-    // c.fill();
+PlayersLabelsFieldViewLayer.prototype.rectByPosition = function(position, chunksScope) {
+    if (this.fieldManager.playersPositions.containsKey(position)) {
+        var name = this.fieldManager.playersPositions.value(position).name;
 
-    c.fillStyle = "white";
-    c.fillText(name, rect.x + 5, rect.y + 15);
+        var c = this.imageData.renderContext;
+        var cellRect = chunksScope.cellRect(position);
+
+        return {x: cellRect.x - 0.5, y: cellRect.y - 20.5, width: c.measureText(name).width + 10, height: 20};
+    }
+};
+
+PlayersLabelsFieldViewLayer.prototype.renderByPosition = function(position, rect) {
+    var hover = pointInRect(rect, this.absoluteMousePositionToRelative(this.mouseListener.mousePos));
+    this.renderedRects.push({rect: rect, hover: hover});
+
+    // console.log('!' + JSON.stringify(this.absoluteMousePositionToRelative(this.mouseListener.mousePos)));
+
+    if (!hover) {
+        // console.log('draw');
+
+        var c = this.imageData.renderContext;
+
+        // todo: check if mouse in rect
+        c.save();
+        c.fillStyle = "rgba(0, 0, 0, 0.5)";
+        c.fillRect(rect.x, rect.y, rect.width, rect.height);
+        c.restore();
+
+        c.fillText(this.fieldManager.playersPositions.value(position).name, rect.x + 4, rect.y + 15);
+    }
+};
+
+var pointInRect = function(rect, point) {
+    return (point.x >= rect.x) && (point.x <= rect.x + rect.width)
+        && (point.y >= rect.y) && (point.y <= rect.y + rect.height);
+};
+
+PlayersLabelsFieldViewLayer.prototype.doOnMouseMove = function(layerMousePosition) {
+    var renderFlag = false;
+    this.renderedRects.forEach(function(renderedRect) {
+        if (!renderedRect.hover && pointInRect(renderedRect.rect, layerMousePosition)) {
+            // renderedRect.hover = true;
+            renderFlag = true;
+
+            // console.log('mouse enter');
+        } else if (renderedRect.hover && !pointInRect(renderedRect.rect, layerMousePosition)) {
+            // renderedRect.hover = false;
+            renderFlag = true;
+
+            // console.log('mouse leave');
+        }
+    });
+
+    if (renderFlag)
+        this.refresh();
 };
